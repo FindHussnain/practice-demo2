2398fe65f4212eae3a223bb18eae7615fa09dbbe
Hussnain Younas <FindHussnain@gmail.com>
2022-04-08 10:53:26 +0500

    user type created

---
+++ app/graphql/types/movie_type.rb
@@ -0,0 +1,4 @@
+module Types
+  class MovieType < Types::BaseObject
+  end
+end
---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,16 @@
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :email, String, null: true
+    field :first_name, String, null: true
+    field :last_name, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :movie_count, Integer, null: true
+    field :movies, [Types::MovieType], null: true
+    def movie_count
+      object.movies.size
+    end
+  end
+end
